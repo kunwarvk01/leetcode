e883bacb780c9c9b87177fb522c6ad6835abd1a7
Vimal Kunwar <72430911+kunwarvk01@users.noreply.github.com>
2023-10-29 23:19:12 +0530

    Time: 11 ms (69.18%), Space: 11.4 MB (17.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp; // val -> index
+
+        for (int i = 0; i < n; i++) {
+            int compliment = target - nums[i];
+            if (mp.find(compliment) != mp.end()) {
+                return {mp[compliment], i};
+            }
+            mp.insert({nums[i], i});
+        }
+        return {};
+    }
+};
